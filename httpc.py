858a4e7c3722ae750319f10f5c15b2840b48fd59
marcleclair <>
2018-10-04 18:24:38 -0400

    finished cli for get

--- httpc.py
+++ httpc.py
@@ -2,16 +2,27 @@
 import sys
 import argparse
 
-parser = argparse.ArgumentParser(prog='httpc', add_help=False)
+parser = argparse.ArgumentParser(prog='httpc', add_help=False,                                  description = 'cURL like functionality to do                            GET and POST requests in python', usage="httpc (get|post) [-v] (-h \"k:v\")* [-d inline-data] [-f file] URL")
 
-parser.add_argument("(get|post)",
-                    help="display a square of a given number")
+parser.add_argument("request" ,default = 'get',
+                    nargs='?',
+                    choices={'get', 'post'},
+                    help="http request chosen to use")
 parser.add_argument("-v", "--verbosity", action="store_true",
                     help="increase output verbosity")
-parser.add_argument("-((h 'k:v'))*", "--headers", action="store_true",
-                    help="headers")
+parser.add_argument('-(h \"k:v\")*',"--headers", nargs='*')
 parser.add_argument('--help', action='help', help='show this help message and exit')
 
 args = parser.parse_args()
-answer = args.square**2
+verbose = False
+requestType = 'get'
+parsed_conf = {}
+if args.request:
+            requestType = args.request
+if args.verbosity:
+            verbose = True
+
+for pair in args.headers:
+    key, value = pair.split('=')
+    parsed_conf[key] = value
 
